679b63fc2269f3d1377bf09c5c5a280ec2eb0a92
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-05-31 23:15:54 +0530

    Added Config and LOg maintaining modules.

--- app/src/main/java/com/ck/dev/rootdemoapp/utils/Config.java
+++ app/src/main/java/com/ck/dev/rootdemoapp/utils/Config.java
@@ -1,4 +1,18 @@
 package com.ck.dev.rootdemoapp.utils;
 
+import android.util.Log;
+
 public class Config {
+
+    private static final boolean DEBUG_MODE = true;
+
+    public static void LOG(String tag, String message, boolean error) {
+        if (DEBUG_MODE)
+            return;
+        if (error)
+            Log.e(tag, message);
+        else
+            Log.d(tag, message);
+    } // LOG
+
 }
