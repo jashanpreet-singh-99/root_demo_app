edcc7e81a842bf31d67e87970606597b2fec039b
Jashanpreet Singh <jashan.singh.work@icloud.com>
2021-05-31 23:54:01 +0530

    Updated HomeScreen Activity UI and added onClick action.

--- app/src/main/java/com/ck/dev/rootdemoapp/activities/HomeScreen.java
+++ app/src/main/java/com/ck/dev/rootdemoapp/activities/HomeScreen.java
@@ -2,11 +2,14 @@ package com.ck.dev.rootdemoapp.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.ck.dev.rootdemoapp.R;
 import com.ck.dev.rootdemoapp.utils.Config;
 
 public class HomeScreen extends Activity {
+
+    private Button checkRootBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,15 @@ public class HomeScreen extends Activity {
 
     private void initView() {
         Config.LOG(Config.TAG_HOME_SCREEN, "Started Home Activity.", false);
+
+        checkRootBtn = this.findViewById(R.id.check_root_btn);
+
+        onClick();
     } // initView
+
+    private void onClick() {
+        checkRootBtn.setOnClickListener( v -> {
+
+        });
+    } // onClick
 }
